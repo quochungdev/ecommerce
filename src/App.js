8367f9667d4d39ca2dbac6747e50ea92f4d2d710
michaelhung123 <zuroken140@gmail.com>
2023-09-26 22:53:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './pages/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import { Carousel, Image } from 'react-bootstrap';
+import CarouselMain from './components/CarouselMain';
+import Register from './components/Register';
+import Products from './components/Products';
+import MyUserReducer from './reducers/MyUserContext';
+import cookie from "react-cookies";
+import MainLayout from './layouts/MainLayout';
+import AdminLayout from './layouts/AdminLayout';
+
+export const MyUserContext = createContext();
+
 
 function App() {
+  const [user, dispatch] = useReducer(MyUserReducer, cookie.load("user") || null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyUserContext.Provider value={[user, dispatch]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<MainLayout />} />
+          <Route path="/admin/*" element={<AdminLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </MyUserContext.Provider>
   );
 }
 
