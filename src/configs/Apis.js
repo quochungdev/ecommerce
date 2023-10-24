85defded40fd34990e21ddc1199d2f82873a546a
Quoc Hung <89074949+quochungdev@users.noreply.github.com>
2023-10-24 20:01:12 +0700

    Update Apis.js

--- src/configs/Apis.js
+++ src/configs/Apis.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import cookie from "react-cookies";
 
 
-const SERVER = "http://localhost:8083";
+const SERVER = "http://localhost:8080";
 
 export const endpoints = {
     "signin": `/api/signin/`,
